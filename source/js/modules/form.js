d86ba1b67ee3288e02df4d0d758fee87d4802c2f
Marina Kuklina <nelmare@yandex.ru>
2022-03-10 07:19:10 +0300

    вносит правки

--- source/js/modules/form.js
+++ source/js/modules/form.js
@@ -4,31 +4,33 @@ export const userEmailInput = form.querySelector('[name=e-mail]');
 export const userPhoneInput = form.querySelector('[name=phone]');
 
 const localStorageDataSet = () => {
-  let isStorageSupport = true;
-  let storageName = '';
-  let storageEmail = '';
-  let storagePhone = '';
+  if (form) {
+    let isStorageSupport = true;
+    let storageName = '';
+    let storageEmail = '';
+    let storagePhone = '';
 
-  try {
-    storageName = localStorage.getItem('name');
-    storageEmail = localStorage.getItem('email');
-    storagePhone = localStorage.getItem('phone');
-  } catch (err) {
-    isStorageSupport = false;
-  }
-
-  form.addEventListener('submit', function (evt) {
-    if (!userNameInput.value || !userEmailInput.value || !userPhoneInput.value) {
-      evt.preventDefault();
-      form.offsetWidth = form.offsetWidth;
-    } else {
-      if (isStorageSupport) {
-        localStorage.setItem('name', userNameInput.value);
-        localStorage.setItem('email', userEmailInput.value);
-        localStorage.setItem('phone', userPhoneInput.value);
-      }
+    try {
+      storageName = localStorage.getItem('name');
+      storageEmail = localStorage.getItem('email');
+      storagePhone = localStorage.getItem('phone');
+    } catch (err) {
+      isStorageSupport = false;
     }
-  });
+
+    form.addEventListener('submit', function (evt) {
+      if (!userNameInput.value || !userEmailInput.value || !userPhoneInput.value) {
+        evt.preventDefault();
+        form.offsetWidth = form.offsetWidth;
+      } else {
+        if (isStorageSupport) {
+          localStorage.setItem('name', userNameInput.value);
+          localStorage.setItem('email', userEmailInput.value);
+          localStorage.setItem('phone', userPhoneInput.value);
+        }
+      }
+    });
+  }
 };
 
 export {localStorageDataSet};
--- source/js/modules/phone-validate.js
+++ source/js/modules/phone-validate.js
@@ -1,18 +1,20 @@
 import {userPhoneInput} from './form';
 
 const userPhoneInputValidate = () => {
-  userPhoneInput.addEventListener('input', () => {
-    userPhoneInput.setCustomValidity('');
-    userPhoneInput.checkValidity();
-  });
+  if (userPhoneInput) {
+    userPhoneInput.addEventListener('input', () => {
+      userPhoneInput.setCustomValidity('');
+      userPhoneInput.checkValidity();
+    });
 
-  userPhoneInput.addEventListener('invalid', () => {
-    if (userPhoneInput.value === '') {
-      userPhoneInput.setCustomValidity('Введите номер телефона');
-    } else {
-      userPhoneInput.setCustomValidity('Введите номер телефона в формате: 123-456-7890');
-    }
-  });
+    userPhoneInput.addEventListener('invalid', () => {
+      if (userPhoneInput.value === '') {
+        userPhoneInput.setCustomValidity('Введите номер телефона');
+      } else {
+        userPhoneInput.setCustomValidity('Введите номер телефона в формате: 123-456-7890');
+      }
+    });
+  }
 };
 
 export {userPhoneInputValidate};
